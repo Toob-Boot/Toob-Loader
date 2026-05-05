f01b68e0fad3b2e8a02f90a14b3499324f6ff642
mannomann <robin.kurth@gmx.de>
2026-05-05 04:51:10 +0200

    del: cleanup

--- cli/suit/test.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-with open('test.cddl', 'w', encoding='utf-8') as f:
-    f.write('''test_map = { my_field }
-my_field = (100: uint)''')
-subprocess.run([r'C:\Espressif\tools\idf-python\3.11.2\Scripts\zcbor.exe', 'code', '-c', 'test.cddl', '--decode', '-t', 'test_map', '--output-c', 'test.c', '--output-h', 'test.h'])
